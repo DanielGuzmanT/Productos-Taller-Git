2950c44233e5103e80b5fb14c3aabe52a8444926
Daniel Guzmán <jhairguz.t@gmail.com>
2019-03-26 14:28:03 -0500

    str a product representation agregado

--- database/models.py
+++ database/models.py
@@ -16,4 +16,4 @@ class Product(Base):
         .format(self.id, self.name, self.price/10, self.description)
 
     def __str__(self):
-        return str(self.id).center(3) + str(self.name).center(20) + str(round(self.price/100, 2)) + str(self.description).center(30)
+        return str(self.id).ljust(3) + str(self.name).ljust(20) + str(round(self.price/100, 2)).ljust(10) + str(self.description).ljust(30)
