87683ae343c29e066f36e3eba75b3dd09eef3780
Daniel Guzmán <jhairguz.t@gmail.com>
2019-03-25 19:59:33 -0500

    main listo para completar

---
+++ main.py
@@ -0,0 +1,15 @@
+# TRABAJO EN EQUIPO PARA TALLER DE GIT Y GITHUB
+# AAII
+
+
+def main():
+    """ Función principal, pregunta al usuario qué desea realizar:
+        - agregar nuevos productos (pedir todos los datos y guardar en base de datos)
+        - listar todos los productos (imprimir todo)
+    """
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
