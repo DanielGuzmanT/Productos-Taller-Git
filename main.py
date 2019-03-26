c26a487a4e8e495a10d80b5bd4c80416e010e8ed
Daniel Guzmán <jhairguz.t@gmail.com>
2019-03-25 20:42:48 -0500

    codigo de 'importar funciones' agregado

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 # TRABAJO EN EQUIPO PARA TALLER DE GIT Y GITHUB
 # AAII
 
+# FUNCIONES DE GUI
+from interfaz.graficas import (
+    gui_presentation,
+    gui_confirm_product_registered,
+    gui_list_products,
+    gui_register_products,
+    gui_show_products_list
+)
+
+# FUNCIONES DE INPUTS
+from interfaz.inputs import (
+    input_chosen_option,
+    input_register_product
+)
 
 def main():
     """ Función principal, pregunta al usuario qué desea realizar:
